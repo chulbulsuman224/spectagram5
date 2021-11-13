d54d20c3ae58c571fd031e3a104f72bcb48b574f
chulbulsuman224 <77154652+chulbulsuman224@users.noreply.github.com>
2021-11-13 10:51:15 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmnuW8xny1Bupe61zDGUP0asBWBPuLUlM",
+    authDomain: "spectagram-app-8b2d9.firebaseapp.com",
+    projectId: "spectagram-app-8b2d9",
+    storageBucket: "spectagram-app-8b2d9.appspot.com",
+    messagingSenderId: "897059878891",
+    appId: "1:897059878891:web:98ed01d96283d272134bae",
+    measurementId: "G-LLFQ47Q39F"
+  }; 
